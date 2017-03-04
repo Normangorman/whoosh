8d813158144ebd8cfe3d9b88577ea5b38582db81
Benjamin Gorman <8076bgorman@gmail.com>
2017-03-04 22:27:28 +0000

    Add is_touching_ground/wall/ceiling methods to PhysicsComponent

--- tests/components/physics_test.py
+++ tests/components/physics_test.py
@@ -3,6 +3,7 @@ import unittest
 from whoosh.components.physics import PhysicsComponent
 from whoosh.blob import Blob
 from whoosh.maths import Vec2f
+from whoosh.gameworld import GameWorld
 
 
 class TestPhysicsComponent(unittest.TestCase):
@@ -17,3 +18,30 @@ class TestPhysicsComponent(unittest.TestCase):
 
         phys.set_position(Vec2f(5, 5))
         self.assertEqual(phys.body.position, (5, 5))
+
+    def test_touching_ground(self):
+        bob = Blob("bob")
+        phys = PhysicsComponent(mass=1, moment=10, body_type=pymunk.Body.DYNAMIC)
+        phys.add_rect(10, 10)
+        phys.set_position(Vec2f(0, 100))
+        bob.add_component(phys)
+
+        wall = Blob("wall")
+        wall_phys = PhysicsComponent(body_type=pymunk.Body.STATIC)
+        wall_phys.add_rect(100, 20)
+        wall_phys.set_position(Vec2f(0, 0))
+        wall.add_component(wall_phys)
+
+        world = GameWorld()
+        world.add_blob(bob)
+        world.add_blob(wall)
+
+        world.update(0.01)
+        self.assertFalse(phys.is_touching_ground())
+
+        for _ in range(50):
+            world.update(0.03)
+            if phys.is_touching_ground():
+                # Passed
+                return
+        self.fail()
--- whoosh/components/physics.py
+++ whoosh/components/physics.py
@@ -19,6 +19,12 @@ class PhysicsComponent(BaseComponent):
         BaseComponent.__init__(self)
         self.body = pymunk.Body(**kwargs)
         self.shapes = []
+        self._touching_ground = False # cache these at the start of every tick
+        self._touching_wall = False
+        self._touching_ceiling = False
+
+    def on_pre_tick(self):
+        self._update_touching_info()
 
     def get_position(self):
         """
@@ -46,6 +52,30 @@ class PhysicsComponent(BaseComponent):
         chipmunk calls our _set_velocity_func.
         """
         self.body.position = pos
+
+    def is_touching_ceiling(self):
+        """
+        Returns True/False whether the body is touching a ceiling
+        """
+        return self._touching_ceiling
+
+    def is_touching_ground(self):
+        """
+        Returns True/False whether the body is touching the ground
+        """
+        return self._touching_ground
+
+    def is_touching_wall(self):
+        """
+        Returns True/False whether the body is touching a wall
+        """
+        return self._touching_wall
+
+    def is_in_air(self):
+        """
+        Returns True/False whether the body is not touching the ground.
+        """
+        return not self.is_touching_ground()
 
     def add_circle(self, radius, **kwargs):
         """
@@ -88,3 +118,34 @@ class PhysicsComponent(BaseComponent):
         verts = whoosh.maths.get_ngon_vertices(radius, sides)
         print("Verts are {0}".format(verts))
         return self.add_poly(verts)
+
+    def _update_touching_info(self):
+        """
+        Sets _touching_ground, _touching_wall and _touching_ceiling by checking all of the current collisions the body is in
+        with other static or kinematic bodies.
+        """
+        def f(arbiter):
+            my_shape = None
+            other_shape = None
+            for shape in arbiter.shapes:
+                if shape.body is self.body:
+                    my_shape = shape
+                else:
+                    other_shape = shape
+            bt = other_shape.body.body_type 
+            if bt == pymunk.Body.STATIC or bt == pymunk.Body.KINEMATIC:
+                nang = arbiter.contact_point_set.normal.angle_degrees
+
+                if 135 > nang and nang > 45: # Pointing upwards
+                    f.touching_ceiling = True
+                elif -135 < nang and nang < -45: # Pointing downwards
+                    f.touching_ground = True
+                else: # Must be pointing sideways
+                    f.touching_wall = True
+        f.touching_ceiling = False
+        f.touching_wall = False
+        f.touching_ground = False
+        self.body.each_arbiter(f)
+        self._touching_ceiling = f.touching_ceiling
+        self._touching_wall = f.touching_wall
+        self._touching_ground = f.touching_ground
